9e4dff4818dffe63f80f9a1495d30d3eeb11d28a
Ekaterina Pykhalova <kausha2339@yandex.ru>
2025-03-14 02:17:48 +0300

    First Review

--- src/pages/register/register.tsx
+++ src/pages/register/register.tsx
@@ -1,18 +1,31 @@
 import { FC, SyntheticEvent, useState } from 'react';
 import { RegisterUI } from '@ui-pages';
+import { useDispatch, useSelector } from '../../services/store';
+import { TRegisterData } from '@api';
+import { getError, register } from '../../slices/user';
 
 export const Register: FC = () => {
+  const dispatch = useDispatch();
+  const error = useSelector(getError);
+
   const [userName, setUserName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
   const handleSubmit = (e: SyntheticEvent) => {
     e.preventDefault();
+    const userData: TRegisterData = {
+      email: email,
+      name: userName,
+      password: password
+    };
+
+    dispatch(register(userData));
   };
 
   return (
     <RegisterUI
-      errorText=''
+      errorText={error}
       email={email}
       userName={userName}
       password={password}
--- src/services/store.ts
+++ src/services/store.ts
@@ -1,22 +1,33 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { ingredientSlice } from '../slices/ingredient';
+import { feedSlice } from '../slices/feed';
+import { newOrderSlice } from '../slices/new_order';
 
 import {
   TypedUseSelectorHook,
   useDispatch as dispatchHook,
   useSelector as selectorHook
 } from 'react-redux';
+import { constructorSlice } from '../slices/constructor';
+import { userSlice } from '../slices/user';
+import { userOrders } from '../slices/orders_list';
 
-const rootReducer = () => {}; // Заменить на импорт настоящего редьюсера
+export const rootReducer = combineReducers({
+  [ingredientSlice.name]: ingredientSlice.reducer,
+  [constructorSlice.name]: constructorSlice.reducer,
+  [userSlice.name]: userSlice.reducer,
+  [feedSlice.name]: feedSlice.reducer,
+  [newOrderSlice.name]: newOrderSlice.reducer,
+  [userOrders.name]: userOrders.reducer
+});
 
 const store = configureStore({
   reducer: rootReducer,
   devTools: process.env.NODE_ENV !== 'production'
 });
 
-export type RootState = ReturnType<typeof rootReducer>;
-
-export type AppDispatch = typeof store.dispatch;
-
+type RootState = ReturnType<typeof store.getState>;
+type AppDispatch = typeof store.dispatch;
 export const useDispatch: () => AppDispatch = () => dispatchHook();
 export const useSelector: TypedUseSelectorHook<RootState> = selectorHook;
 
